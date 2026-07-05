50fccf4f478b90a2111d66b6cf5471192340972a
sanaullah-devopes <sanaullah963741@gmail.com>
2026-07-05 06:12:03 -0700

    basic objects of products and profile

---
+++ store-product.js
@@ -0,0 +1,27 @@
+/*  this is the code of store product */
+
+const product ={
+    name: "ball pen",
+    rating: 5,
+    price : 380,
+    offer: 5,
+};
+
+console.log(product);
+
+/* This is the instagram profile code */
+
+const profile = {
+    name: "@Alyas mushtak",
+    isfollow: true,
+    post : 400,
+    followers : 3489,
+    folloing :837,
+};
+
+console.log(profile);
+
+console.log(typeof profile);
+console.log(typeof profile["name"]);
+console.log( profile.name);
+

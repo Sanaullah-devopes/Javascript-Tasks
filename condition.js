e6222618edebaf36236f2542ba868748b890359b
sanaullah-devopes <sanaullah963741@gmail.com>
2026-07-06 00:34:09 -0700

    If and else if

---
+++ condition.js
@@ -0,0 +1,46 @@
+
+//          condition statement
+
+let age =11;
+
+if (age >18 ) {
+    console.log("You can vote");
+}else {
+
+    console.log("you can't vate");
+}
+//                   dark mofe in website 
+
+let mode ="dark";
+let color;
+if (mode==="dark"){
+    color ="black";
+}
+if (mode === "light"){
+    color = "white";
+}
+console.log(color);
+
+//              check even & old number 
+
+let  num = 49;
+if (num%2===0){
+    console.log(num ,"The number is even");
+}else {
+    console.log(num ,"The number is old");   
+}
+
+//                else if statement
+
+if (num ===29){
+    console.log("number found");
+} else if(num ===30){
+    console.log("number found");
+}else{
+    console.log("number not same");
+
+}
+//                    ternary operators
+
+let number =38;
+ number>18 ? console.log("Adult") : console.log(" Not Adult");

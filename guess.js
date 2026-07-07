fc2ce96b77ad611a4329ea26b26d92ff4886e303
sanaullah-devopes <sanaullah963741@gmail.com>
2026-07-06 18:44:10 -0700

    Number guessing game

---
+++ guess.js
@@ -0,0 +1,7 @@
+let num= 56;
+let guess=prompt("Enter any random number :");;
+do{
+   guess=prompt("you entered wrong number. Try again  :");
+   
+} while (guess!=num);
+console.log ("Congratulation");

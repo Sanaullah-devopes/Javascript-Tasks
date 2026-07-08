8c222911e532c915f41fd73b357af85683cc5b17
sanaullah-devopes <sanaullah963741@gmail.com>
2026-07-08 03:32:55 -0700

    solution of this question

---
+++ practice.js
@@ -0,0 +1,8 @@
+
+/* Prompt the user to enter their full name. Generate a username for them based on the input.
+Start username with @, followed by their full name and ending with the fullname length. 
+eg: user name = “shradhakhapra” , username should be “@shradhakhapra13 */
+
+let  fullname =prompt("Enter your name :");
+let name = "@" + fullname + fullname.length; 
+console.log(name);
